ce23b77fecfc49b945687c93babbdda6390f0abb
Eduardo Lima <eduardostudio@gmail.com>
2024-04-18 09:51:29 -0300

    test: add new tests cases for user

--- internal/domain/user_test.go
+++ internal/domain/user_test.go
@@ -19,14 +19,29 @@ func Test_If_Get_Error_If_UserName_Is_Not_Valid(t *testing.T) {
 	assert.Error(t, err, "username must has at least 5 characters")
 }
 
+func Test_If_No_Error_If_UserName_Is_Valid(t *testing.T) {
+	_, err := NewUser(idUser, "eduardolima806", displayName, email, password)
+	assert.Nil(t, err, "The user name is valid")
+}
+
 func Test_If_Get_Error_If_Email_Is_Not_Valid(t *testing.T) {
 	_, err := NewUser(idUser, userName, displayName, "invalidmail.com", password)
 	assert.Error(t, err, "email is not valid")
 }
 
+func Test_If_No_Error_If_Email_Is_Valid(t *testing.T) {
+	_, err := NewUser(idUser, userName, displayName, "validmail@company.com", password)
+	assert.Nil(t, err, "email is valid")
+}
+
 func Test_If_Get_Error_If_Password_Is_Not_Secure(t *testing.T) {
 	_, err := NewUser(idUser, userName, displayName, email, "password123")
 	assert.Error(t, err, "password is not secure")
+}
+
+func Test_If_No_Error_If_Password_Is_Not_Secure(t *testing.T) {
+	_, err := NewUser(idUser, userName, displayName, email, "P4$$word")
+	assert.Nil(t, err, "password is secure")
 }
 
 func Test_If_Get_No_Error_For_Valid_User(t *testing.T) {
