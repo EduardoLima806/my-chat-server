3bba196f43a0c9ed589c5a186f8940e31b02cc49
Eduardo Lima <eduardostudio@gmail.com>
2024-04-24 17:36:07 -0300

    chore: remove unused files and code

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/eduardolima806/my-chat-server/config"
 	"github.com/eduardolima806/my-chat-server/internal/app"
-	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -17,47 +15,4 @@ func main() {
 	}
 
 	app.Run(cgf)
-	// api.Server()
-}
-
-func createAndLoginUserCmd() {
-	fmt.Println("Hello My Chat Server!")
-
-	loadEnvFile()
-
-	// dbConfig := db.CreateDbConfigFromEnv(*db.NewEnvDBConfig())
-	// conn, err := db.ConnectToPostgresDb(*dbConfig)
-	// if err != nil {
-	// 	log.Fatal("Error to connect to database!", err)
-	// }
-	// defer conn.Close()
-
-	// userRepo := repository.NewUserRepository(conn)
-	// passwordHasher := &util.DefaultPasswordHasher{}
-	// createUserUc := usecase.NewCreateUserUseCase(userRepo, passwordHasher)
-	// input := usecase.UserInput{UserName: "eduardolima806", DisplayName: "Eduardo Lima", Email: "eduardolima.dev.io@gmail.com", Password: "P4$$w0rd001"}
-	// output, err := createUserUc.Execute(input)
-	// if err != nil {
-	// 	fmt.Printf("Error to create user: %s. %s\n", input.UserName, err)
-	// } else {
-	// 	fmt.Printf("User %s created with the id %d.\n", input.UserName, output.CreatedUserId)
-	// }
-	// loginInput := usecase.LoginInput{Login: "eduardolima806", Password: "P4$$w0rd001"}
-	// loginUserUc := usecase.NewLoginUserUseCase(userRepo, passwordHasher)
-	// loginOutput, err := loginUserUc.Execute(loginInput)
-	// if err != nil {
-	// 	log.Fatalf("Error to login user: %s. %s", loginInput.Login, err)
-	// }
-	// if loginOutput.IsSucceed {
-	// 	fmt.Printf("Succeed login for user %s", loginInput.Login)
-	// } else {
-	// 	fmt.Printf("Error to login for user %s. Err: %d", loginInput.Login, loginOutput.ErrorType)
-	// }
-}
-
-func loadEnvFile() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 }
